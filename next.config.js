38c0d7823e4beaf9b3e28e8111adc55e239fed7a
Rizky <rizky.arifin00@gmail.com>
2022-01-27 19:56:37 +0700

    modify next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  output: { path: path.resolve(__dirname, 'static'), },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.pdf/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/[hash][ext]',
+      },
+    })
+
+    return config
+},
 }
